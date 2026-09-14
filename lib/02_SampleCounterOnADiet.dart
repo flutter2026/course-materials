52c4c252dc2eefcebb287600e3edf13278f33633
barryburd <bburd@drew.edu>
2026-09-13 21:27:09 -0400

    Slimmed down the Sample Counter app.

--- lib/02_SampleCounterOnADiet.dart
+++ lib/02_SampleCounterOnADiet.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: .fromSeed(seedColor: Colors.green),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
